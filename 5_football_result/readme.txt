fd32b37fe1adf1d0ff78830b85a0b54210b646c8
Shivesh Tiwari <stshandilya0820@gmail.com>
2022-07-09 15:16:34 +0530

    reading and analysing csv file

--- 5_football_result/readme.txt
+++ 5_football_result/readme.txt
@@ -1,2 +1,5 @@
 commit #4:
   football stats project setup
+
+commit #5:
+  reading and analysing csv file
--- 5_football_result/src/index.ts
+++ 5_football_result/src/index.ts
@@ -1,1 +1,15 @@
-console.log('football stats')
+import fs = require('fs');
+
+const matches = fs.readFileSync('football.csv', {
+  encoding: 'utf-8'
+})
+.split('\n')
+.map((row: string): string[] => row.split(','))
+
+let manUnitedWins = 0;
+for(const match of matches){
+    manUnitedWins += (match[1] === 'Man United' && match[5] === 'H') ? 1 : 0; 
+    manUnitedWins += (match[2] === 'Man United' && match[5] === 'A') ? 1 : 0;
+}
+
+console.log(manUnitedWins)
