3f948d9667f22baa4aa3618255afbdbd424a7910
Shivesh Tiwari <yuvanastra@Shiveshs-MacBook-Air.local>
2022-07-09 13:06:33 +0530

    added support for characters array and linkedList

--- 4_sort/readme.txt
+++ 4_sort/readme.txt
@@ -1,6 +1,15 @@
+commit #1:
   get length(): number{
     return this.data.length
   }
 
   we don't need to call length
   this.collection.length is the way to get length
+
+commit #2:
+  till now, Sorter class only accepts NumbersCollection,
+  but now, we'll make it such that it accepts any collection of data, as long as
+  the collection have:
+    compare(i, j)
+    swap(i, j)
+    length
--- 4_sort/src/index.ts
+++ 4_sort/src/index.ts
@@ -1,8 +1,24 @@
+import { CharacteresCollection } from './CharactersCollection'
+import { LinkedList } from './LinkedListCollection'
 import { NumbersCollection } from './NumbersCollection'
 import { Sorter } from './Sorter'
 
-const numbersCollection = new NumbersCollection([10, 7, 12, -5])
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+// const numbersCollection = new NumbersCollection([10, 7, 12, -5])
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data)
 
-console.log(numbersCollection.data)
+// const charactersCollection = new CharacteresCollection('azaXabY');
+// const sorter = new Sorter(charactersCollection)
+// sorter.sort()
+// console.log(charactersCollection.data)
+
+const linkedList = new LinkedList();
+linkedList.add(10)
+linkedList.add(7)
+linkedList.add(12)
+linkedList.add(-5)
+
+const sorter = new Sorter(linkedList)
+sorter.sort()
+linkedList.print()
